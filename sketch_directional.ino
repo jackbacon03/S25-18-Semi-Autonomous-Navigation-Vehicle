deea9d7deedcb68db5ca03ebd86f0177a09be498
Jack Bacon <111783056+jackbacon03@users.noreply.github.com>
2024-11-05 14:58:07 -0500

    Updates to Code

--- sketch_directional.ino
+++ sketch_directional.ino
@@ -1,34 +1,63 @@
 // Prototype Testing
 
-int M2D2 = 13;
-int M2IN1 = 12;
-int M2IN2 = 8;
-int M2D1 = 4;
+// FRONT LEFT
+int M1D1  = 52;
+int M1IN1 = 50;
+int M1D2  = 10;
+int M1IN2 = 51;
 
-// int M2D3 = ;
-// int M2D4 = ;
-// int M2IN3 = ;
-// int M2IN4 = ;
+// FRONT RIGHT
+int M2D1  = 33;
+int M2IN1 = 30;
+int M2D2  = 8;
+int M2IN2 = 31;
 
-int EN = 7;
+// BACK LEFT
+// int M3D1  = ;
+// int M3IN1 = ;
+// int M3D2  = ;
+// int M3IN2 = ;
 
+// BACK RIGHT
+// int M4D1  = ;
+// int M4IN1 = ;
+// int M4D2  = ;
+// int M4IN2 = ;
+
+// ENABLE
+int EN = 32;
+
+// DIRECTION
 String direction = "FORWARD"; // Change this to "FORWARD", "RIGHT", "BACKWARD", or "LEFT"
 
-// Test Timer
+// TIMER
 unsigned long startTime = 0;
-const unsigned long duration = 30000; // 30 seconds 
+const unsigned long duration = 60000; // 60 Seconds 
 
+// PIN MAPPING
 void setup() {
-  pinMode(M2D2, OUTPUT);
+  pinMode(M1D1,  OUTPUT);
+  pinMode(M1IN1, OUTPUT);
+  pinMode(M1D2,  OUTPUT);
+  pinMode(M1IN2, OUTPUT);
+
+  pinMode(M2D1,  OUTPUT);
   pinMode(M2IN1, OUTPUT);
+  pinMode(M2D2,  OUTPUT);
   pinMode(M2IN2, OUTPUT);
-  pinMode(M2D1, OUTPUT);
 
-  // pinMode(M2D3, OUTPUT);
-  // pinMode(M2D4, OUTPUT);
-  // pinMode(M2IN3, OUTPUT);
-  // pinMode(M2IN4, OUTPUT);
+  /*
+  pinMode(M3D1,  OUTPUT);
+  pinMode(M3IN1, OUTPUT);
+  pinMode(M3D2,  OUTPUT);
+  pinMode(M3IN2, OUTPUT);
 
+  pinMode(M4D1,  OUTPUT);
+  pinMode(M4IN1, OUTPUT);
+  pinMode(M4D2,  OUTPUT);
+  pinMode(M4IN2, OUTPUT);
+  */
+  
   pinMode(EN, OUTPUT);
 
   startTime = millis();
@@ -52,66 +81,135 @@ void loop() {
 
 // FORWARD
 void moveForward() {
-  digitalWrite(M2D2, HIGH);
-  digitalWrite(M2IN1, HIGH);
-  digitalWrite(M2D1, LOW);
-  digitalWrite(M2IN2, LOW);
-  // digitalWrite(M2D3, );
-  // digitalWrite(M2IN4, );
-  // digitalWrite(M2D3, );
-  // digitalWrite(M2IN4, );
+  digitalWrite(M1D1,  LOW);
+  digitalWrite(M1IN1, HIGH);
+  digitalWrite(M1D2,  HIGH);
+  digitalWrite(M1IN2, LOW);
+  
+  digitalWrite(M2D1,  LOW);
+  digitalWrite(M2IN1, LOW);
+  digitalWrite(M2D2,  HIGH);
+  digitalWrite(M2IN2, HIGH);
+
+  /*
+  digitalWrite(M3D1,  );
+  digitalWrite(M3IN1, );
+  digitalWrite(M3D2,  );
+  digitalWrite(M3IN2, );
+
+  digitalWrite(M4D1,  );
+  digitalWrite(M4IN1, );
+  digitalWrite(M4D2,  );
+  digitalWrite(M4IN2, );
+  */
+
   digitalWrite(EN, HIGH);
 }
 
+
 // BACKWARD
 void moveBackward() {
-  digitalWrite(M2D2, );
+  /*
+  digitalWrite(M1D1,  );
+  digitalWrite(M1IN1, );
+  digitalWrite(M1D2,  );
+  digitalWrite(M1IN2, );
+  
+  digitalWrite(M2D1,  );
   digitalWrite(M2IN1, );
-  digitalWrite(M2D1, );
+  digitalWrite(M2D2,  );
   digitalWrite(M2IN2, );
-  digitalWrite(M2D3, );
-  digitalWrite(M2IN4, );
-  digitalWrite(M2D3, );
-  digitalWrite(M2IN4, );
+
+  digitalWrite(M3D1,  );
+  digitalWrite(M3IN1, );
+  digitalWrite(M3D2,  );
+  digitalWrite(M3IN2, );
+
+  digitalWrite(M4D1,  );
+  digitalWrite(M4IN1, );
+  digitalWrite(M4D2,  );
+  digitalWrite(M4IN2, );
+
   digitalWrite(EN, HIGH);
+  */
 }
 
 // RIGHT
 void turnRight() {
-  digitalWrite(M2D2, );
+  /*
+  digitalWrite(M1D1,  );
+  digitalWrite(M1IN1, );
+  digitalWrite(M1D2,  );
+  digitalWrite(M1IN2, );
+  
+  digitalWrite(M2D1,  );
   digitalWrite(M2IN1, );
-  digitalWrite(M2D1, );
+  digitalWrite(M2D2,  );
   digitalWrite(M2IN2, );
-  digitalWrite(M2D3, );
-  digitalWrite(M2IN4, );
-  digitalWrite(M2D3, );
-  digitalWrite(M2IN4, );
+
+  digitalWrite(M3D1,  );
+  digitalWrite(M3IN1, );
+  digitalWrite(M3D2,  );
+  digitalWrite(M3IN2, );
+
+  digitalWrite(M4D1,  );
+  digitalWrite(M4IN1, );
+  digitalWrite(M4D2,  );
+  digitalWrite(M4IN2, );
+
   digitalWrite(EN, HIGH);
+  */
 }
 
 // LEFT
 void turnLeft() {
-  digitalWrite(M2D2, );
+  /*
+  digitalWrite(M1D1,  );
+  digitalWrite(M1IN1, );
+  digitalWrite(M1D2,  );
+  digitalWrite(M1IN2, );
+  
+  digitalWrite(M2D1,  );
   digitalWrite(M2IN1, );
-  digitalWrite(M2D1, );
+  digitalWrite(M2D2,  );
   digitalWrite(M2IN2, );
-  digitalWrite(M2D3, );
-  digitalWrite(M2IN4, );
-  digitalWrite(M2D3, );
-  digitalWrite(M2IN4, );
+
+  digitalWrite(M3D1,  );
+  digitalWrite(M3IN1, );
+  digitalWrite(M3D2,  );
+  digitalWrite(M3IN2, );
+
+  digitalWrite(M4D1,  );
+  digitalWrite(M4IN1, );
+  digitalWrite(M4D2,  );
+  digitalWrite(M4IN2, );
+
   digitalWrite(EN, HIGH);
+  */
 }
 
 // STOP
 void stopMotor() {
-  digitalWrite(M2D2, LOW);
+  digitalWrite(M1D1,  LOW);
+  digitalWrite(M1IN1, LOW);
+  digitalWrite(M1D2,  LOW);
+  digitalWrite(M1IN2, LOW);
+  
+  digitalWrite(M2D1,  LOW);
   digitalWrite(M2IN1, LOW);
-  digitalWrite(M2D1, LOW);
+  digitalWrite(M2D2,  LOW);
   digitalWrite(M2IN2, LOW);
-  digitalWrite(M2D3, LOW);
-  digitalWrite(M2IN4, LOW);
-  digitalWrite(M2D3, LOW);
-  digitalWrite(M2IN4, LOW);
+
+  digitalWrite(M3D1,  LOW);
+  digitalWrite(M3IN1, LOW);
+  digitalWrite(M3D2,  LOW);
+  digitalWrite(M3IN2, LOW);
+
+  digitalWrite(M4D1,  LOW);
+  digitalWrite(M4IN1, LOW);
+  digitalWrite(M4D2,  LOW);
+  digitalWrite(M4IN2, LOW);
+
   digitalWrite(EN, LOW);
 }
 
--- sketch_oct16a.ino
+++ sketch_oct16a.ino
@@ -1,28 +1,94 @@
 
-int M2D2 = 13;
-int M2IN1 = 12;
-int M2IN2 = 8;
-int M2D1 = 4;
-int EN = 7;
+int M2D2 = 8;
+int M2IN1 = 30;
+int M2IN2 = 31;
+int M2D1 = 33;
+
+int EN = 32;
+
+int M1IN1 = 50;
+int M1IN2 = 51;
+int M1D1 = 52;
+int M1D2 = 10;
+
 
 
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(M2D2, OUTPUT);
+  pinMode(M2D2, OUTPUT); 
   pinMode(M2IN1, OUTPUT);
   pinMode(M2IN2, OUTPUT);
   pinMode(M2D1, OUTPUT);
+
   pinMode(EN, OUTPUT);
 
+  pinMode(M1D1, OUTPUT); 
+  pinMode(M1IN1, OUTPUT);
+  pinMode(M1IN2, OUTPUT);
+  pinMode(M1D2, OUTPUT);
 }
 
 void loop() {
+
+ digitalWrite(EN, HIGH);
  
- digitalWrite(M2D2, HIGH);
+ analogWrite(M2D2, 255);
  digitalWrite(M2IN1, HIGH);
  digitalWrite(M2D1, LOW);
  digitalWrite(M2IN2, LOW);
+
+ analogWrite(M1D2, 255);
+ digitalWrite(M1IN1, LOW);
+ digitalWrite(M1D1, LOW);
+ digitalWrite(M1IN2, HIGH);
+
+ delay(2000);
+
+
  digitalWrite(EN, HIGH);
+ 
+ analogWrite(M2D2, 200);
+ digitalWrite(M2IN1, HIGH);
+ digitalWrite(M2D1, LOW);
+ digitalWrite(M2IN2, LOW);
+
+ analogWrite(M1D2, 200);
+ digitalWrite(M1IN1, LOW);
+ digitalWrite(M1D1, LOW);
+ digitalWrite(M1IN2, HIGH);
+
+ delay(2000);
+
+
+ digitalWrite(EN, HIGH);
+ 
+ analogWrite(M2D2, 127);
+ digitalWrite(M2IN1, HIGH);
+ digitalWrite(M2D1, LOW);
+ digitalWrite(M2IN2, LOW);
+
+ analogWrite(M1D2, 127);
+ digitalWrite(M1IN1, LOW);
+ digitalWrite(M1D1, LOW);
+ digitalWrite(M1IN2, HIGH);
+
+ 
+ delay(2000);
+
+
+ digitalWrite(EN, HIGH);
+ 
+ analogWrite(M2D2, 60);
+ digitalWrite(M2IN1, HIGH);
+ digitalWrite(M2D1, LOW);
+ digitalWrite(M2IN2, LOW);
+
+ analogWrite(M1D2, 60);
+ digitalWrite(M1IN1, LOW);
+ digitalWrite(M1D1, LOW);
+ digitalWrite(M1IN2, HIGH);
+
+ delay(2000);
 
 }
